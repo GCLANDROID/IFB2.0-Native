bd1a083565b93de0b2d079ffd4060b1446336e2d
GCLANDROID <104360689+sahaarpan5@users.noreply.github.com>
2026-03-31 14:43:50 +0530

    31 mar

--- app/src/main/java/io/cordova/ifb/activity/LoginActivity.java
+++ app/src/main/java/io/cordova/ifb/activity/LoginActivity.java
@@ -143,24 +143,24 @@ public class LoginActivity extends AppCompatActivity {
 
         refreshedToken = "1222";
 
-        android_id= "263268b3b9658c47";
-        /*android_id = getAndroidID(LoginActivity.this);
-        if (android_id.equals("0")) {
-            TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            android_id = telephonyManager.getDeviceId();
-        }else {
-            android_id = getAndroidID(LoginActivity.this);
-        }*/
+        android_id= "7c1af6037f2b729c";
+ //       android_id = getAndroidID(LoginActivity.this);
+//        if (android_id.equals("0")) {
+//            TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+//            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+//                // TODO: Consider calling
+//                //    ActivityCompat#requestPermissions
+//                // here to request the missing permissions, and then overriding
+//                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                //                                          int[] grantResults)
+//                // to handle the case where the user grants the permission. See the documentation
+//                // for ActivityCompat#requestPermissions for more details.
+//                return;
+//            }
+//            android_id = telephonyManager.getDeviceId();
+//        }else {
+//            android_id = getAndroidID(LoginActivity.this);
+//        }
 
         tvShow = (TextView) findViewById(R.id.tvShow);
         tvHide = (TextView) findViewById(R.id.tvHide);
